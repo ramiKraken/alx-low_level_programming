5061c6abb07473981e54e64e3ecb26cc5f0a17f9
ramiKraken <ramyahmad21804@gmail.com>
2023-10-08 17:17:14 -0700

    putting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *the program puts output to standard i/o
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
