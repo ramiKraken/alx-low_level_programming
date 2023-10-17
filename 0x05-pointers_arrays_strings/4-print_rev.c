9c12c42204cc81615984dbae212810bb0b58f68b
ramiKraken <ramyahmad21804@gmail.com>
2023-10-16 17:42:54 -0700

    point oint

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,8 @@ void print_rev(char *s)
 	int count = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
-		count ++;
-	for (i = count -1 ; i >= 0 ; i--)
+		count++;
+	for (i = count - 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
