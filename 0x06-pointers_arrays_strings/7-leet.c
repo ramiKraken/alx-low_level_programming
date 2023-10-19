eabbff21b50c971c9244b759dea415ce952eeac0
ramiKraken <ramyahmad21804@gmail.com>
2023-10-19 12:49:41 -0700

    kk

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - function encodes a string into 1337
+ * @str: strings no be encoded
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char c[] = "aAeEoOTlL";
+	char d[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+			}
+		}
+	}
+	return (str);
+}
