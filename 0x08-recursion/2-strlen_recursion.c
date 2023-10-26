2ad14a6e3e72e57faa31d8144e6fdcc2682b675a
ramiKraken <ramyahmad21804@gmail.com>
2023-10-25 17:19:48 -0700

    jjk

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Calculate length of string
+ * @s: string to be counted
+ * Return: int value;
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
