57fce2bc15529ec46d0569fd0eed3d377fdc450c
ramiKraken <ramyahmad21804@gmail.com>
2023-11-08 10:37:53 -0800

    search for int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - search for integer
+  * @array: var1
+  * @size: var2
+  * @cmp: var3
+  *
+  * Return: -1
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
